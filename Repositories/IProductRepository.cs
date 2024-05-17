71f7fb1c1d389967128eccf1fa80414ede1fda6c
lucasfogacadj <lucasfgc00@gmail.com>
2024-05-17 11:47:33 -0300

    first commit

---
+++ Repositories/IProductRepository.cs
@@ -0,0 +1,8 @@
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAll();
+    Task<Product> GetById(int id);
+    Task Add(Product product);
+    Task Update(Product product);
+    Task Delete(int id);
+}
